7f9b56bf87e83ac9a302482a5974cb39eb8f39eb
tom <1359801580@qq.com>
2018-12-12 23:56:57 +0800

    12/12

--- 03-js/js/util.js
+++ 03-js/js/util.js
@@ -1,34 +1,4 @@
-function animate(obj,attr,target){
-		clearInterval(obj.timer);
-		var iSpeed =0;
-		obj.timer=setInterval(function(){
-			var current =parseFloat(getComputedStyle(obj,false)[attr]);
-			if (attr=='opacity') {
-				current=Math.round(current*100);
-			}
-			// current=current*100;
-			if(current>target){
-				iSpeed=-80;
-			}else{
-				iSpeed=80;
-			}
-			if (Math.abs(target-current)<Math.abs(iSpeed)) {
-				if (attr=='opacity') {
-					obj.style.opacity=target/100;
-				}else{
-					obj.style[attr]=target+'px';
-				}
-				obj.style.opacity=target/100;
-				clearInterval(obj.timer);
-			}else{
-				if (attr=='opacity') {
-				obj.style.opacity=(current+iSpeed)/100;
-				}else{
-				obj.style[attr]=current+iSpeed+'px';
-				}				
-			}			
-		},30)	
-	}
+
 /**
 * @param  {object} obj    [DOM节点对象]
 * @param  {options[attr]} options   [属性和目标值的对象]
@@ -113,4 +83,7 @@ obj.timer = setInterval(function(){
 }
 function getScrollTop(){
 	return window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop;
+}
+function getScrollLeft(){
+	return window.pageXOffset||document.documentElement.scrollLeft||document.body.scrollLeft;
 }
